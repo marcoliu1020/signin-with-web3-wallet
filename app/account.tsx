5b9d1a2810fc290b0f122f0836e7104627cad12c
marco <a0931900802@gmail.com>
2025-04-21 22:22:34 +0800

    feat: sign in with metamask

--- app/account.tsx
+++ app/account.tsx
@@ -1,13 +1,29 @@
+'use client'
+
 import { useAccount, useDisconnect } from 'wagmi'
 
+// context
+import { signInPipeWork } from '@/context/signin-pipe-work'
+import { metamaskAuthFlow } from '@/context/wagmi/metamask-auth-flow'
+
 export function Account() {
-  const { address } = useAccount()
+  const { address, chainId } = useAccount()
   const { disconnect } = useDisconnect()
 
+  const handleSignIn = async () => {
+    if (!address || !chainId) {
+      throw new Error('Address or chainId is not set')
+    }
+    const signInFlow = signInPipeWork(address, chainId)
+    const result = await signInFlow(metamaskAuthFlow)
+    console.log(result)
+  }
+
   return (
-    <div>
+    <div className='flex flex-col gap-4'>
       {address && <div>{address}</div>}
       <button onClick={() => disconnect()}>Disconnect</button>
+      <button onClick={handleSignIn}> Sign in </button>
     </div>
   )
 }
