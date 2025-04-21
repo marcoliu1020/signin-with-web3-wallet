dad642415e2ec7da44dc0252628cac2c26234651
marco <a0931900802@gmail.com>
2025-04-21 22:21:33 +0800

    feat: create metamaskAuthFlow

---
+++ context/wagmi/metamask-auth-flow.ts
@@ -0,0 +1,28 @@
+import { signMessage } from '@wagmi/core';
+import { config } from './config';
+
+// api
+import { getNonce } from '@/api/get-nonce';
+import { getUserToken } from '@/api/get-user-token';
+
+// type
+import type { AuthenticationAdapter } from "../signin-pipe-work";
+
+export const metamaskAuthFlow: AuthenticationAdapter = {
+    getNonce: async ({ address }) => {
+      const nonce = await getNonce({ address, chainType: 'ETHEREUM' }); // metamask 都是 ethereum
+      return nonce;
+    },
+    createSignature: async ({ nonce }) => {
+      const signature = await signMessage(config, { message: nonce });
+      return signature;
+    },
+    verifySignature: async ({ address, signature }) => {
+      const userToken = await getUserToken({ address, chainType: 'ETHEREUM', signature }); // metamask 都是 ethereum
+      console.log(userToken)
+      return userToken.success;
+    },
+    signOut: async () => {
+      // TODO: Implement sign out
+    }
+  }
