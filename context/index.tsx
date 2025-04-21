c06992474cd37513cc86d921e1651073134d15eb
marco <a0931900802@gmail.com>
2025-04-21 20:16:34 +0800

    mv: config to wagmi folder

--- context/index.tsx
+++ context/index.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// wagmi
 import { WagmiProvider } from 'wagmi';
-import { config as wagmiConfig } from "./config/wagmi";
+import { config } from "./wagmi/config";
 
 const queryClient = new QueryClient()
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
