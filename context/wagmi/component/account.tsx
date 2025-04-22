d27c8dd3f97a0ea78799eb2865400ac1b255100c
marco <a0931900802@gmail.com>
2025-04-22 16:24:29 +0800

    feat: add try catch on handleSignIn

--- context/wagmi/component/account.tsx
+++ context/wagmi/component/account.tsx
@@ -1,24 +1,34 @@
 'use client'
 
+import { useState } from 'react'
 import { useAccount, useDisconnect } from 'wagmi'
 
-// context
+// signin-pipe-work
 import { signInPipeWork } from '@/context/signin-pipe-work'
 import { evmAuthFlow } from '@/context/wagmi/evm-auth-flow'
 
 export function Account() {
-  const { address, chainId } = useAccount()
   const { disconnect } = useDisconnect()
+  const { address, chainId } = useAccount()
+  const [authStatus, setAuthStatus] = useState<'unauthenticated' | 'authenticated' | 'loading'>('unauthenticated')
 
+  // this is effect event
   const handleSignIn = async () => {
-    if (!address || !chainId) {
-      throw new Error('Address or chainId is not set')
-    }
-    const signInFlow = signInPipeWork(address, chainId)
-    const result = await signInFlow(evmAuthFlow)
-    if (result.success) {
+    if (authStatus === 'authenticated' || authStatus === 'loading') return
+    if (!address || !chainId) throw new Error('Address or chainId is not set')
+
+    try {
+      setAuthStatus('loading')
+      const signInFlow = signInPipeWork(address, chainId)
+      const result = await signInFlow(evmAuthFlow)
+      if (!result.success) {
+        throw new Error('Sign in failed')
+      }
+      setAuthStatus('authenticated')
       alert('sign in success')
-    } else {
+    } catch (error) {
+      console.error(error)
+      setAuthStatus('unauthenticated')
       alert('sign in failed')
     }
   }
@@ -27,7 +37,7 @@ export function Account() {
     <div className='flex flex-col gap-4'>
       {address && <div>{address}</div>}
       <button onClick={() => disconnect()} className='border border-gray-300 rounded-md p-2'>Disconnect</button>
-      <button onClick={handleSignIn} className='border border-gray-300 rounded-md p-2'> Sign in </button>
+      <button onClick={handleSignIn} className='border border-gray-300 rounded-md p-2'>Sign in</button>
     </div>
   )
 }
