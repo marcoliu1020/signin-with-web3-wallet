4792903101f47fc703cbf43abf4082cabfc6f3e1
marco <a0931900802@gmail.com>
2025-04-22 18:16:52 +0800

    feat: add EVM provider

--- context/index.tsx
+++ context/index.tsx
@@ -2,18 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// wagmi
-import { WagmiProvider } from 'wagmi';
-import { config } from "./evm/config";
+// evm
+import { EVMContextProvider } from "./evm";
 
 const queryClient = new QueryClient()
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
+    <EVMContextProvider>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
-    </WagmiProvider>
+    </EVMContextProvider>
   )
 }
