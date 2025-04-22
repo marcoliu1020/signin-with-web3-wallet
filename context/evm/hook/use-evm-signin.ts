453322bf55d6f8201d871b33eeda0043bc893442
marco <a0931900802@gmail.com>
2025-04-22 18:26:53 +0800

    refactor: move file to hook

--- context/evm/hook/use-evm-signin.ts
+++ context/evm/hook/use-evm-signin.ts
@@ -2,11 +2,22 @@
 
 import { useState } from "react"
 import { useAccount } from "wagmi"
+import { signMessage } from '@wagmi/core';
 
 // signin-pipe-work
-import { signInPipeWork } from '@/context/signin-pipe-work'
-import { evmAuthAdapter } from '@/context/evm/evm-auth-adapter'
+import { signInPipeWork, type AuthenticationAdapter } from '@/context/signin-pipe-work'
 
+// config
+import { config } from '@/context/evm/config';
+
+// api
+import { getNonce } from '@/api/get-nonce';
+import { getUserToken } from '@/api/get-user-token';
+
+// util
+import { setUserToken } from '@/util/user-token';
+
+/** evm 錢包登入 */
 export function useEvmSignin() {
     const { address, chainId } = useAccount()
     const [authStatus, setAuthStatus] = useState<'unauthenticated' | 'authenticated' | 'loading'>('unauthenticated')
@@ -36,3 +47,27 @@ export function useEvmSignin() {
         handleEvmSignIn,
     }
 }
+
+/** evm 錢包認證適配器 */
+export const evmAuthAdapter: AuthenticationAdapter = {
+    getNonce: async ({ address }) => {
+      const nonce = await getNonce({ address, chainType: 'ETHEREUM' }); // metamask 都是 ethereum
+      return nonce;
+    },
+    createSignature: async ({ nonce }) => {
+      // 可以用這個網站測試 https://etherscan.io/verifiedSignatures#
+      const signature = await signMessage(config, { message: nonce });
+      return signature;
+    },
+    verifySignature: async ({ address, signature }) => {
+      const userToken = await getUserToken({ address, chainType: 'ETHEREUM', signature }); // metamask 都是 ethereum
+      if (userToken.data.isSuccess) {
+        // TODO: 儲存 userToken
+        setUserToken(userToken.data.token)
+      }
+      return userToken.data.isSuccess;
+    },
+    signOut: async () => {
+      // TODO: Implement sign out
+    }
+  }
