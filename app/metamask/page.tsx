27a7b97b4f8437b47d6dfb0dc9eb4269c74d3261
marco <a0931900802@gmail.com>
2025-04-22 17:55:57 +0800

    feat: handle sign in with backend

--- app/metamask/page.tsx
+++ app/metamask/page.tsx
@@ -1,10 +1,19 @@
 'use client'
 
+import { useEffect } from 'react'
 import { useAccount } from 'wagmi'
+
+// component
 import { Account } from '@/context/wagmi/component/account'
 import { WalletOptions } from '@/context/wagmi/component/wallet-options'
 
-export default function Home() {
+// hook
+import { useEvmSignin } from '@/context/wagmi/hook/use-evm-signin'
+
+// util
+import { getUserToken } from '@/util/user-token'
+
+export default function MetamaskPage() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <ConnectWallet />
@@ -14,6 +23,28 @@ export default function Home() {
 
 function ConnectWallet() {
   const { isConnected } = useAccount()
+  const { authStatus, handleEvmSignIn } = useEvmSignin()
+
+  useEffect(() => {
+    // 如果已經有 userToken，則不進行 signin
+    const userToken = getUserToken()
+    if (userToken) {
+      console.log('userToken', userToken)
+      return
+    }
+
+    // 執行 signin
+    (async () => {
+      if (isConnected && authStatus === 'unauthenticated') {
+        const hasSignedIn = await handleEvmSignIn()
+        if (hasSignedIn) {
+          alert('signed in')
+        } else {
+          alert('signed in failed')
+        }
+      }
+    })()
+  }, [isConnected, authStatus, handleEvmSignIn])
 
   if (isConnected) return <Account />
   return <WalletOptions />
